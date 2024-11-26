08569f79c2a8a34a07ebc0d76c8d7e64c2486bce
BuiTuanPhong <buituanphong1501@gmail.com>
2024-11-26 22:49:15 +0700

    Complete Ex2

---
+++ Lab3/AimsProject/AimsProject.java
@@ -0,0 +1,35 @@
+public class AimsProject{
+    public static void main(String[] args){
+        Cart a1 = new Cart();
+        DigitalVideoDisc dvd1= new DigitalVideoDisc("abc","def", "ghi", 12, 19.95f);
+        a1.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("hfgs", "jsfgsjf", "sjdfsj", 75, 24.95f);
+        a1.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3= new DigitalVideoDisc("sfgjsdf", "jsgfjsdfgs", "hgsfshgf", 25, 18.99f);
+        a1.addDigitalVideoDisc(dvd3);
+        DigitalVideoDisc dvd4= new DigitalVideoDisc("sfgjsgdf", "jsgfjshdfgs", "hggsfshgf", 225, 15.99f);
+        a1.addDigitalVideoDisc(dvd4);
+
+        DigitalVideoDisc[] dvdList = new DigitalVideoDisc[2];
+        dvdList[0]= new DigitalVideoDisc("fgjf","fbejf","fgwfj", 14,13.5f);
+        dvdList[1]= new DigitalVideoDisc("gfj","fwf","jhfwj",23,15f);
+
+        a1.addDigitalVideoDisc(dvdList);
+        
+        a1.addDigitalVideoDisc(dvd1, dvd2);
+
+        System.out.println(dvd1.getId());
+        System.out.println(dvd2.getId());
+        System.out.println(dvd3.getId());
+        System.out.println(dvd4.getId());
+
+         
+        System.out.print("Total cost is: ");
+        System.out.println(a1.totalCost());
+        a1.removeDigitalVideoDisc(dvd2);
+        System.out.print("Total cost is: ");
+        System.out.println(a1.totalCost());
+    }
+}
+
+
---
+++ Lab3/AimsProject/Cart.java
@@ -0,0 +1,110 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 21;
+
+    int qtyOrder=0;
+
+    private DigitalVideoDisc itemsOdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED]; 
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrder==20){
+            System.out.println("Gio hang da day!!!");
+        }
+        else {
+            for(int i=1;i<=itemsOdered.length;i++){
+                if(itemsOdered[i]==null){
+                    itemsOdered[i]=disc;
+                    qtyOrder+=1;
+                    break;
+                }
+            }
+            System.out.println("Da them vao gio hang!!!");
+            
+        }
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc1, DigitalVideoDisc disc2){
+        if(qtyOrder==19){
+            System.out.println("Gio hang da day!!!");
+        }
+        else{
+            for(int i=1;i<itemsOdered.length;i++){
+                if(itemsOdered[i]==null){
+                    itemsOdered[i]=disc1;
+                    itemsOdered[i+1]=disc2;
+                    qtyOrder+=2;
+                    break;
+                }
+            }
+            System.out.println("Da them 2 sam pham!!!");
+        }
+    }
+        
+
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList){
+        if(qtyOrder + dvdList.length >= 21){
+            System.out.println("Gio hang day do danh sach lon!!");
+        }
+        else{
+        for(int i=1; i<=itemsOdered.length; i++){
+            if(itemsOdered[i]==null){
+
+                System.arraycopy(dvdList, 0, itemsOdered,i, dvdList.length );
+                qtyOrder+=dvdList.length;
+                break;
+
+            }
+            
+        }
+        System.out.println("Da them vao gio hang danh sach !!");
+        }
+    }
+
+
+     public void addDigitalVideoDisc(DigitalVideoDisc... discs){
+        
+        for(DigitalVideoDisc disc:discs){
+
+        
+        if(qtyOrder==20){
+            System.out.println("Gio hang da day!!!");
+        }
+        else {
+            for(int i=1;i<=itemsOdered.length;i++){
+                if(itemsOdered[i]==null){
+                    itemsOdered[i]=disc;
+                    qtyOrder+=1;
+                    break;
+                }
+            }
+            System.out.println("Da them vao gio hang!!!");
+            
+        }
+    }
+    }
+
+    public float totalCost(){
+        float sum = 0.0f;
+        for(int i = 1 ; itemsOdered[i]!= null && i<itemsOdered.length ; i++){
+            sum+= itemsOdered[i].getCost();
+        }
+        return sum;
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        for(int i=1;i<=itemsOdered.length;i++){
+            if(itemsOdered[i].equals(disc)){
+                for (int j = i; j < itemsOdered.length - 1; j++) {
+                    itemsOdered[j] = itemsOdered[j + 1];
+                }
+                
+                itemsOdered[itemsOdered.length - 1] = null;
+                qtyOrder--;
+                System.out.println("Da xoa thanh cong");
+                break;
+                
+            }
+            
+        }
+
+    }
+}
