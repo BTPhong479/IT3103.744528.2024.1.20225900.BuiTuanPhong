647b784b0c9663181e7b43ee9542664658d90f62
BuiTuanPhong <buituanphong1501@gmail.com>
2024-12-01 21:08:23 +0700

    complete8

--- Lab4/AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
+++ Lab4/AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
@@ -1,50 +1,31 @@
 package AimsProject.src.hust.soict.dsai.aims.store.Store;
 
 import AimsProject.src.hust.soict.dsai.aims.disc.DigitalVideoDisc.DigitalVideoDisc;
+import AimsProject.src.hust.soict.dsai.aims.media.Media;
+import java.util.ArrayList;
+
 
 public class Store {
 
-    public static final int MAX_NUMBERS_STORE = 1000;
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_NUMBERS_STORE];
-
-    public void addDVD(DigitalVideoDisc disc){
-        
-        
-            for(int i=1;i<=itemsInStore.length;i++){
-                if(itemsInStore[i]==null){
-                    itemsInStore[i]=disc;
-                    
-                    break;
-                }
-            }
-            System.out.println("Da them vao cua hang!!!");
-            
-        
+    public void addMedia(Media media){
+        itemsInStore.add(media);
+        System.out.println("Da them vao cua hang!!!");
     }
 
-
-    public void removeDVD(DigitalVideoDisc disc){
-        for(int i=1;i<=itemsInStore.length;i++){
-            if(itemsInStore[i].equals(disc)){
-                for (int j = i; j < itemsInStore.length - 1; j++) {
-                    itemsInStore[j] = itemsInStore[j + 1];
-                }
-                
-                itemsInStore[itemsInStore.length - 1] = null;
-                
-                System.out.println("Da xoa thanh cong");
-                break;
-                
-            }
-            
-        }
-
+    public void removeMedia(Media media){
+        if(itemsInStore.contains(media)) {
+            itemsInStore.remove(media);
+            System.out.println("Da xoa khoi cua hang!!!");
+        }else System.out.println("Media tren ko co trong cua hang");
     }
+
+}
 
 
 
 
 
     
-}
+
